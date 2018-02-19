cb174f9e2189f45010eb7d2e0bb54f1bb4ef6231
Okrash0 <35527883+Okrash0@users.noreply.github.com>
2018-02-19 11:57:33 +0100

    Add files via upload

---
+++ MånadsPeng.js
@@ -0,0 +1,45 @@
+
+var totKr;
+
+var loadKr = localStorage.getItem('ml');
+
+window.onload = function() {
+  if (loadKr >= 0) {
+    totKr = Number(loadKr);
+  }
+  
+  else {
+   totKr = 0;
+  }
+  document.getElementById('tot').innerHTML = totKr + ' ' + 'ml';
+};
+
+
+
+function tioKr() {
+  totKr += 700;
+  document.getElementById('tot').innerHTML = totKr + ' ' + 'ml';
+  localStorage.setItem('Kr', totKr);
+}
+
+
+function reset() {
+  var conf = confirm('Vill du verkligen nollställa?');
+  if (conf) {
+    totKr = 0;
+    document.getElementById('tot').innerHTML = totKr + ' ' + 'ml';
+    localStorage.setItem('Kr', totKr);
+  }
+}
+
+function costum() {
+  var costNum = document.getElementById('cost').value;
+  totKr += Number(costNum);
+  document.getElementById('tot').innerHTML = totKr + ' ' + 'ml';
+    
+  if (totKr < 0) {
+    totKr = 0;
+    document.getElementById('tot').innerHTML = totKr + ' ' + 'ml';
+  }
+  localStorage.setItem('Kr', totKr);
+}
